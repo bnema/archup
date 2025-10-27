1549b62fbd008eea152abfa7ac1ad0418f452084
bnema <b@bnema.dev>
2025-10-27 20:35:12 +0100

    refactor(tests): centralize test helper functions to avoid duplication

---
+++ internal/phases/helpers_test.go
@@ -0,0 +1,26 @@
+package phases
+
+import (
+	"os"
+	"time"
+)
+
+// containsSubstring checks if a string contains a substring
+func containsSubstring(s, substr string) bool {
+	for i := 0; i < len(s)-len(substr)+1; i++ {
+		if s[i:i+len(substr)] == substr {
+			return true
+		}
+	}
+	return false
+}
+
+// mockFileInfo implements os.FileInfo for testing
+type mockFileInfo struct{}
+
+func (m mockFileInfo) Name() string       { return "mock" }
+func (m mockFileInfo) Size() int64        { return 0 }
+func (m mockFileInfo) Mode() os.FileMode  { return 0 }
+func (m mockFileInfo) ModTime() time.Time { return time.Now() }
+func (m mockFileInfo) IsDir() bool        { return false }
+func (m mockFileInfo) Sys() interface{}   { return nil }
